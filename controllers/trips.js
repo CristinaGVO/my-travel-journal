0ae8b6de5fee39aa42f362affabfb866cf3bec75
CristinaGVO <vargasosoriocristina@gmail.com>
2025-12-14 17:57:42 -0800

    Add more funcionality

--- controllers/trips.js
+++ controllers/trips.js
@@ -17,9 +17,23 @@ router.get('/', async (req, res) => {
 });
 
 
-
 router.get('/new', async (req, res) => {
   res.render('trips/new.ejs');
+});
+
+// Show
+
+router.get('/:tripId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const trip = currentUser.trips.id(req.params.tripId);
+    res.render('trips/show.ejs', {
+    trip: trip,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 
@@ -38,6 +52,51 @@ router.post('/', async (req, res) => {
 });
 
 
+// delete
+
+router.delete('/:tripId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.trips.id(req.params.tripId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/trips`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// editar -update informacion
+
+router.put('/:tripId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const trip = currentUser.trips.id(req.params.tripId);
+    trip.set(req.body);
+    await currentUser.save();
+    res.redirect(
+      `/users/${currentUser._id}/trips/${req.params.tripId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+//editar el trip
+
+router.get('/:tripId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const trip = currentUser.trips.id(req.params.tripId);
+    res.render('trips/edit.ejs', {
+      trip: trip,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
 
 module.exports = router;
